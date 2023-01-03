663cfada8d2a9c7f8764daa719cf07e24d7965ac
Torres-09 <dla612@naver.com>
2023-01-04 02:47:31 +0900

    Initial commit

---
+++ src/main/java/com/example/naverloginexample/NaverloginexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.naverloginexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverloginexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverloginexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/naverloginexample/NaverloginexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.naverloginexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverloginexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
